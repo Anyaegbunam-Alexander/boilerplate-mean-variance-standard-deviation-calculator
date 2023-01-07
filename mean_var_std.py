e236b3d3f7595851113fdc448be5fb32e3b7d53f
Anyaegbunam-Alexander <alexanderking.aa@gmail.com>
2023-01-07 02:56:37 +0100

    Updated Code

--- mean_var_std.py
+++ mean_var_std.py
@@ -20,24 +20,24 @@ def calculate(list):
         std_axis2 = [float(x) for x in three_x_matrx.std(axis=0)]
 
         max_flat = flat_matrix.max()
-        max_axis1 = three_x_matrx.max(axis=1)
-        max_axis2 = three_x_matrx.max(axis=0)
+        max_axis1 = [x for x in three_x_matrx.max(axis=1)]
+        max_axis2 = [x for x in three_x_matrx.max(axis=0)]
 
         min_flat = flat_matrix.min()
-        min_axis1 = three_x_matrx.min(axis=1)
-        min_axis2 = three_x_matrx.min(axis=0)
+        min_axis1 = [x for x in three_x_matrx.min(axis=1)]
+        min_axis2 = [x for x in three_x_matrx.min(axis=0)]
 
         sum_flat = flat_matrix.sum()
-        sum_axis1 = three_x_matrx.sum(axis=1)
-        sum_axis2 = three_x_matrx.sum(axis=0)
+        sum_axis1 = [x for x in three_x_matrx.sum(axis=1)]
+        sum_axis2 = [x for x in three_x_matrx.sum(axis=0)]
 
         calculations = {
             'mean': [mean_axis2, mean_axis1, mean_flat],
             'variance': [var_axis2, var_axis1, var_flat],
             'standard deviation': [std_axis2, std_axis1, std_flat],
-            'max': [[x for x in max_axis2], [x for x in max_axis1], max_flat],
-            'min': [[x for x in min_axis2], [x for x in min_axis1], min_flat],
-            'sum': [[x for x in sum_axis2], [x for x in sum_axis1], sum_flat]
+            'max': [max_axis2, max_axis1, max_flat],
+            'min': [min_axis2, min_axis1, min_flat],
+            'sum': [sum_axis2, sum_axis1, sum_flat]
             }
 
     return calculations
